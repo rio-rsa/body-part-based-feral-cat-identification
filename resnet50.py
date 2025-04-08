c03f6331fea674e634fa5057b06879b6672565d7
Rio R <40910296+rio-rsa@users.noreply.github.com>
2025-04-08 14:18:22 +0800

    Update resnet50.py

--- resnet50.py
+++ resnet50.py
@@ -1,3 +1,5 @@
+#------------------------------------------------------------------------------------------------------ Setup -------------------------------------------------------------------------------------------------------
+
 import numpy as np
 import tensorflow as tf
 from tensorflow import keras
@@ -24,6 +26,8 @@ body_dir = pathlib.Path(body_path)
 front_leg_dir = pathlib.Path(front_leg_path)
 back_leg_dir = pathlib.Path(back_leg_path)
 tail_dir = pathlib.Path(tail_path)
+
+#--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 #--------------------------------------------------------------------------------------- Training, Testing & Validation Split ---------------------------------------------------------------------------------------
 
@@ -109,12 +113,12 @@ back_leg_val_test_ds = tf.keras.preprocessing.image_dataset_from_directory(
   image_size=(img_height, img_width),
   batch_size=batch_size)
 
-# Further split the validation + test set into 50% validation, 50% test
+#--------------------- Further split the validation + test set into 50% validation, 50% test
 back_leg_val_batches = tf.data.experimental.cardinality(back_leg_val_test_ds) // 2
 back_leg_val_ds = back_leg_val_test_ds.take(back_leg_val_batches)
 back_leg_test_ds = back_leg_val_test_ds.skip(back_leg_val_batches)
 
-# Verify the splits
+#--------------------- Verify the splits
 print('Training batches:', tf.data.experimental.cardinality(back_leg_train_ds).numpy())
 print('Validation batches:', tf.data.experimental.cardinality(back_leg_val_ds).numpy())
 print('Test batches:', tf.data.experimental.cardinality(back_leg_test_ds).numpy())
@@ -139,36 +143,39 @@ tail_val_test_ds = tf.keras.preprocessing.image_dataset_from_directory(
   image_size=(img_height, img_width),
   batch_size=batch_size)
 
-# Further split the validation + test set into 50% validation, 50% test
+#--------------------- Further split the validation + test set into 50% validation, 50% test
 tail_val_batches = tf.data.experimental.cardinality(tail_val_test_ds) // 2
 tail_val_ds = tail_val_test_ds.take(tail_val_batches)
 tail_test_ds = tail_val_test_ds.skip(tail_val_batches)
 
-# Verify the splits
+#--------------------- Verify the splits
 print('Training batches:', tf.data.experimental.cardinality(tail_train_ds).numpy())
 print('Validation batches:', tf.data.experimental.cardinality(tail_val_ds).numpy())
 print('Test batches:', tf.data.experimental.cardinality(tail_test_ds).numpy())
 
-# Get class names
+#--------------------- Get class names
 front_leg_class_names = front_leg_train_ds.class_names
 body_class_names = body_train_ds.class_names
 back_leg_class_names = back_leg_train_ds.class_names
 tail_class_names = tail_train_ds.class_names
 
-# Extracting Features
+#--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+#------------------------------------------------------------------------------------------ Extracting and saving features ------------------------------------------------------------------------------------------
+#--------------------- Method to create the structure of the ResNet50 feature extractor
 def create_base_model(name_prefix):
     base_model = tf.keras.applications.ResNet50(include_top=False, weights='imagenet', input_shape=(180, 180, 3), pooling='avg')
 
-    # Create the model using the base model with pooling='avg'
+#--------------------- Create the model using the base model with pooling='avg'
     model = Model(inputs=base_model.input, outputs=base_model.output)
 
-    # Rename the layers to ensure uniqueness
+#--------------------- Renaming layers
     for layer in model.layers:
         layer._name = name_prefix + '_' + layer.name
 
     return model
 
-# Create the separate models
+#--------------------- Separate ResNet50 Feature Extractors
 model_body = create_base_model('resnet_body')
 model_front_leg = create_base_model('resnet_front_leg')
 model_back_leg = create_base_model('resnet_back_leg')
@@ -181,18 +188,14 @@ def extract_features(model, dataset, class_names):
     for images, label in dataset:
         image_batches.append(images)  # Keep the images in case of visualization
 
-        # Extract features
+#--------------------- Extracting features
         feature = model.predict(images)
         features.append(feature)
         labels.append(label)
     features = np.concatenate(features)
     labels = np.concatenate(labels)
     return features, labels, image_batches
-#--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-#------------------------------------------------------------------------------------------ Extracting and saving features ------------------------------------------------------------------------------------------
-
-#--------------------- Extracting and saving features from each body part
 #--------------------- If you remove a body part from above you must also remove the code here
 body_train_features, body_train_labels, body_train_images = extract_features(model_body, body_train_ds, body_class_names)
 body_val_features, body_val_labels, body_val_images = extract_features(model_body, body_val_ds, body_class_names)
@@ -210,7 +213,7 @@ tail_train_features, tail_train_labels, tail_train_images = extract_features(mod
 tail_val_features, tail_val_labels, tail_val_images = extract_features(model_tail, tail_val_ds, tail_class_names)
 tail_test_features, tail_test_labels, tail_test_images = extract_features(model_tail, tail_test_ds, tail_class_names)
 
-# Combine features
+#--------------------- Combining features from all body parts
 train_features = np.concatenate([body_train_features, front_leg_train_features, back_leg_train_features, tail_train_features], axis=-1)
 val_features = np.concatenate([body_val_features, front_leg_val_features, back_leg_val_features, tail_val_features], axis=-1)
 test_features = np.concatenate([body_test_features, front_leg_test_features, back_leg_test_features, tail_test_features], axis=-1)
@@ -219,19 +222,24 @@ train_labels = body_train_labels
 val_labels = body_val_labels
 test_labels = body_test_labels
 
-# Define the final model
+#--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+#--------------------------------------------------------------------------------------------- Model training & testing ---------------------------------------------------------------------------------------------
 input_shape = train_features.shape[1:]
 
+#--------------------- Initialising the softmax classification layer
 model = Sequential([
     Dense(10, activation='softmax')
 ])
 
 model.compile(optimizer=Adam(learning_rate=0.001), loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
-# Train the model
+#--------------------- Training the softmax classification layer with the concatenated features
 model.fit(train_features, train_labels, validation_data=(val_features, val_labels), epochs=10)
 
-# Evaluate the model
+#--------------------- Evaluating the model
 loss, accuracy = model.evaluate(test_features, test_labels)
 print(f'Test accuracy: {accuracy}')
 print(f'Test Loss: {loss}')
+
+#--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
